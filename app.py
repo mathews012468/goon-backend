b1fec42dbdab8679b611ddb1c497cbf9a0b64d1b
mathews012468 <Mathew.Soto78@myhunter.cuny.edu>
2023-01-21 09:57:38 -0500

    combine frontend and backend in one server

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
 from flask import request
+from flask import send_from_directory
+from flask import render_template
 import translateAcronym as ta
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
+def entry():
+    return render_template("index.html")
+
+@app.route('/hello')
+def a():
+    return "Hello"
+
+@app.route('/translate', methods=["GET"])
 def translate():
     #the only url argument should be acronym
     goonAcronym = request.args.get('acronym', default="")
@@ -13,4 +23,3 @@ def translate():
     #restrict to the first ten results
     goonAcronymDefinitionsList = goonAcronymDefined[:10]
     return {"Definitions": [goonWord["goon definition"] for goonWord in goonAcronymDefinitionsList]}
-
